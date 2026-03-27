8ee836ef917789656b9836a314215b4531bad6bd
luiszink <157110320+luiszink@users.noreply.github.com>
2026-03-27 22:46:27 +0100

    feat(controller): add history navigation, clock integration and timed games

--- src/main/scala/chess/controller/ControllerInterface.scala
+++ src/main/scala/chess/controller/ControllerInterface.scala
@@ -1,11 +1,12 @@
 package chess.controller
 
-import chess.model.{Game, Move, ChessError}
+import chess.model.{Game, Move, ChessError, MoveEntry, ChessClock, TimeControl}
 import chess.util.Observer
 
 trait ControllerInterface:
   def game: Game
   def newGame(): Unit
+  def newGameWithClock(timeControl: Option[TimeControl]): Unit
   def doMove(move: Move): Boolean
   def doMoveResult(move: Move): Either[ChessError, Game]
   def loadFen(fen: String): Boolean
@@ -15,3 +16,15 @@ trait ControllerInterface:
   def statusText: String
   def add(observer: Observer): Unit
   def remove(observer: Observer): Unit
+  // History navigation
+  def browseBack(): Unit
+  def browseForward(): Unit
+  def browseToStart(): Unit
+  def browseToEnd(): Unit
+  def browseToMove(index: Int): Unit
+  def isAtLatest: Boolean
+  def browseIndex: Int
+  def gameStatesCount: Int
+  // Clock
+  def clock: Option[ChessClock]
+  def tickClock(): Unit
